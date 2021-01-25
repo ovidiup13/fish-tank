4b6dbf925261d5f34882475362e48c8e50f0a540
Ovidiu Popoviciu <devovip13@gmail.com>
2021-01-25 20:59:48 +0200

    Add validation to add fish form

--- src/components/FishImage/FishImage.jsx
+++ src/components/FishImage/FishImage.jsx
@@ -1,67 +1,25 @@
-import React from 'react';
-
-import { ReactComponent as Fish0 } from '../../assets/fish/fish-0.svg';
-import { ReactComponent as Fish1 } from '../../assets/fish/fish-1.svg';
-import { ReactComponent as Fish2 } from '../../assets/fish/fish-2.svg';
-import { ReactComponent as Fish3 } from '../../assets/fish/fish-3.svg';
-import { ReactComponent as Fish4 } from '../../assets/fish/fish-4.svg';
-import { ReactComponent as Fish5 } from '../../assets/fish/fish-5.svg';
-import { ReactComponent as Fish6 } from '../../assets/fish/fish-6.svg';
-import { ReactComponent as Fish7 } from '../../assets/fish/fish-7.svg';
-import { ReactComponent as Fish8 } from '../../assets/fish/fish-8.svg';
-import { ReactComponent as Fish9 } from '../../assets/fish/fish-9.svg';
-import { ReactComponent as Fish10 } from '../../assets/fish/fish-10.svg';
-import { ReactComponent as Fish11 } from '../../assets/fish/fish-11.svg';
-import { ReactComponent as Fish12 } from '../../assets/fish/fish-12.svg';
-import { ReactComponent as Fish13 } from '../../assets/fish/fish-13.svg';
-import { ReactComponent as Fish14 } from '../../assets/fish/fish-14.svg';
-import { ReactComponent as Fish15 } from '../../assets/fish/fish-15.svg';
-import { ReactComponent as Fish16 } from '../../assets/fish/fish-16.svg';
-import { ReactComponent as Fish17 } from '../../assets/fish/fish-17.svg';
-import { ReactComponent as Fish18 } from '../../assets/fish/fish-18.svg';
-import { ReactComponent as Fish19 } from '../../assets/fish/fish-19.svg';
-import { ReactComponent as Fish20 } from '../../assets/fish/fish-20.svg';
-import { ReactComponent as Fish21 } from '../../assets/fish/fish-21.svg';
-import { ReactComponent as Fish22 } from '../../assets/fish/fish-22.svg';
-import { ReactComponent as Fish23 } from '../../assets/fish/fish-23.svg';
-import { ReactComponent as Fish24 } from '../../assets/fish/fish-24.svg';
-
-const images = {
-  0: Fish0,
-  1: Fish1,
-  2: Fish2,
-  3: Fish3,
-  4: Fish4,
-  5: Fish5,
-  6: Fish6,
-  7: Fish7,
-  8: Fish8,
-  9: Fish9,
-  10: Fish10,
-  11: Fish11,
-  12: Fish12,
-  13: Fish13,
-  14: Fish14,
-  15: Fish15,
-  16: Fish16,
-  17: Fish17,
-  18: Fish18,
-  19: Fish19,
-  20: Fish20,
-  21: Fish21,
-  22: Fish22,
-  23: Fish23,
-  24: Fish24,
-};
+import React, { useState, useEffect } from 'react';
 
 const FishImage = ({ type, ...props }) => {
-  const FishLogo = images[type];
+  const [icon, setIcon] = useState(null);
 
-  if (!FishLogo) {
+  useEffect(() => {
+    // There is a bug in webpack with dynamically loading svgs.
+    // https://github.com/facebook/create-react-app/issues/5276#issuecomment-665628393
+    import(
+      `!!@svgr/webpack?-svgo,+titleProp,+ref!../../assets/fish/fish-${type}.svg`
+    ).then((data) => {
+      setIcon(data);
+    });
+  });
+
+  if (!icon) {
     return null;
   }
 
-  return <FishLogo {...props} />;
+  const { default: Image } = icon;
+
+  return <Image {...props} />;
 };
 
 export default FishImage;
--- src/pages/AddFish/AddFish.jsx
+++ src/pages/AddFish/AddFish.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Formik, Field, Form, useField, useFormikContext } from 'formik';
+import * as Yup from 'yup';
 import { enGB } from 'date-fns/locale';
 import { DatePicker } from 'react-nice-dates';
 import { useHistory } from 'react-router-dom';
@@ -16,12 +17,26 @@ import dayjs from 'dayjs';
 var customParseFormat = require('dayjs/plugin/customParseFormat');
 dayjs.extend(customParseFormat);
 
+const AddFishSchema = Yup.object().shape({
+  name: Yup.string()
+    .max(15, 'Keep it short!')
+    .required('Enter a name for your new fish'),
+  species: Yup.string().required("What's your fish species?"),
+  dob: Yup.string()
+    .test('is-dob', 'Please enter a valid date', (value) =>
+      dayjs(value, DATE_FORMAT).isValid()
+    )
+    .required('When was your fish born?'),
+  lifetime: Yup.number().required("What's the usual lifetime of this fish?"),
+  unit: Yup.string().required('Well... how many what?'),
+});
+
 const AddFish = ({ onFishAdded }) => {
   const history = useHistory();
 
   const initialValues = {
     name: '',
-    species: '',
+    species: SPECIES[0],
     dob: dayjs().format(DATE_FORMAT).toString(),
     lifetime: 10,
     unit: TIME_UNITS[4],
@@ -36,89 +51,122 @@ const AddFish = ({ onFishAdded }) => {
       avatar: Math.floor(Math.random() * 25),
       name: values.name,
       species: values.species,
+
       dob: dayjs(values.dob, DATE_FORMAT).unix() * 1000,
       lifetime: {
         value: values.lifetime,
         unit: values.unit,
       },
+      isAlive: true,
     });
     history.push('/');
+  };
+
+  const validateForm = ({ dob, lifetime, unit }) => {
+    const errors = {};
+    const dateOfBirth = dayjs(dob, DATE_FORMAT);
+
+    if (dateOfBirth.add(lifetime, unit) - dayjs() <= 0) {
+      errors.lifetime = 'Looks like your fish is past its lifetime.';
+    }
+
+    return errors;
   };
 
   return (
     <div className={styles.page}>
       <h1>Add new fish</h1>
-      <Formik initialValues={initialValues} onSubmit={onSubmit}>
-        <Form className={styles.form}>
-          <div className={styles.control}>
-            <label className={styles.label} htmlFor='name'>
-              Fish Name
-            </label>
-            <Field
-              className={styles.field}
-              id='name'
-              name='name'
-              placeholder='Fishy'
-              autoComplete='off'
-            />
-          </div>
-
-          <div className={styles.control}>
-            <label className={styles.label} htmlFor='species'>
-              Species
-            </label>
-            <Field
-              className={`${styles.field} ${styles.select}`}
-              id='species'
-              as='select'
-              name='species'
-              placeholder='Fish'
-            >
-              {SPECIES.map((sp) => (
-                <option key={sp} value={sp}>
-                  {capitalize(sp)}
-                </option>
-              ))}
-            </Field>
-          </div>
-
-          <div className={styles.control}>
-            <label className={styles.label} htmlFor='dob'>
-              Date of birth
-            </label>
-            <Picker name='dob' />
-          </div>
-
-          <div className={styles.control}>
-            <label className={styles.label} htmlFor='dob'>
-              Lifetime
-            </label>
-            <div className={styles.lifetime}>
+      <Formik
+        initialValues={initialValues}
+        onSubmit={onSubmit}
+        validationSchema={AddFishSchema}
+        validate={validateForm}
+      >
+        {({ errors, touched }) => (
+          <Form className={styles.form}>
+            <div className={styles.control}>
+              <label className={styles.label} htmlFor='name'>
+                Fish Name
+              </label>
               <Field
-                className={`${styles.field} ${styles.lifetime_value}`}
-                type='number'
-                name='lifetime'
+                className={styles.field}
+                id='name'
+                name='name'
+                placeholder='Fishy'
+                autoComplete='off'
               />
+              {errors.name && touched.name ? (
+                <small className={styles.error}>{errors.name}</small>
+              ) : null}
+            </div>
+
+            <div className={styles.control}>
+              <label className={styles.label} htmlFor='species'>
+                Species
+              </label>
               <Field
                 className={`${styles.field} ${styles.select}`}
-                id='unit'
+                id='species'
                 as='select'
-                name='unit'
-                placeholder='Time Unit'
+                name='species'
+                placeholder='Fish'
               >
-                {TIME_UNITS.map((unit) => (
-                  <option key={unit} value={unit}>
-                    {capitalize(unit)}
+                {SPECIES.map((sp) => (
+                  <option key={sp} value={sp}>
+                    {capitalize(sp)}
                   </option>
                 ))}
               </Field>
+              {errors.species && touched.species ? (
+                <small className={styles.error}>{errors.species}</small>
+              ) : null}
             </div>
-          </div>
 
-          <Button className={styles.submit} type='submit'>
-            Submit
-          </Button>
-        </Form>
+            <div className={styles.control}>
+              <label className={styles.label} htmlFor='dob'>
+                Date of birth
+              </label>
+              <Picker name='dob' />
+              {errors.dob && touched.dob ? (
+                <small className={styles.error}>{errors.dob}</small>
+              ) : null}
+            </div>
+
+            <div className={styles.control}>
+              <label className={styles.label} htmlFor='dob'>
+                Lifetime
+              </label>
+              <div className={styles.lifetime}>
+                <Field
+                  className={`${styles.field} ${styles.lifetime_value}`}
+                  type='number'
+                  name='lifetime'
+                />
+                <Field
+                  className={`${styles.field} ${styles.select}`}
+                  id='unit'
+                  as='select'
+                  name='unit'
+                  placeholder='Time Unit'
+                >
+                  {TIME_UNITS.map((unit) => (
+                    <option key={unit} value={unit}>
+                      {capitalize(unit)}
+                    </option>
+                  ))}
+                </Field>
+              </div>
+              {errors.lifetime &&
+              (touched.dob || touched.lifetime || touched.unit) ? (
+                <small className={styles.error}>{errors.lifetime}</small>
+              ) : null}
+            </div>
+
+            <Button className={styles.submit} type='submit'>
+              Submit
+            </Button>
+          </Form>
+        )}
       </Formik>
     </div>
   );
@@ -133,7 +181,7 @@ const Picker = (props) => {
       minimumDate={dayjs().subtract(3, 'years').toDate()}
       maximumDate={dayjs().toDate()}
       onDateChange={(date) => {
-        setFieldValue(field.name, dayjs(date).format(DATE_FORMAT).toString());
+        setFieldValue(field.name, dayjs(date).format(DATE_FORMAT));
       }}
       locale={enGB}
     >
@@ -143,6 +191,7 @@ const Picker = (props) => {
           input: !focused,
           'input-focused': focused,
         });
+        console.log({ inputProps });
         return (
           <input
             className={inputClassName}
